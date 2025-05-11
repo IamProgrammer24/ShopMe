4075b1480895b24fa1d4902d8b3cf000ae4c9095
Brajesh <281092322419e@gmail.com>
2025-05-11 19:50:11 +0530

    original code

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import { IoMdSearch } from "react-icons/io";
 import { FaCartShopping } from "react-icons/fa6";
 import { FaCaretDown } from "react-icons/fa";
-import DarkMode from "./DarkMode";
 import { FiShoppingBag } from "react-icons/fi";
 
 const Menu = [
@@ -87,11 +86,6 @@ const Navbar = ({ handleOrderPopup }) => {
               </span>
               <FaCartShopping className="text-xl text-white drop-shadow-sm cursor-pointer" />
             </button>
-
-            {/* Darkmode Switch */}
-            <div>
-              <DarkMode />
-            </div>
           </div>
         </div>
       </div>
